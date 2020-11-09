434316c9c04f5e085583a1c62a66d18ab53c7040
Hamilkar247 <adonnibal96@gmail.com>
2020-11-09 13:28:22 +0100

    WO-2 mugole-niemieckie-slowka stan na 9.11.2020

--- HannonBash.sh
+++ HannonBash.sh
@@ -2,10 +2,15 @@
 
 function discord-text-channel-download
 {
-   dotnet /home/matball/Projects/HannonDiscordChatExporter/DiscordChatExporter.Cli.dll export -t   NTkzNzU2MTk0NDM5NjkyMzAw.X6RG0w.CMSNbqjw_rY04vdOlSgff9PdxbQ -c "$1"
+   dotnet /home/matball/Projects/HannonDiscordChatExporter/DiscordChatExporter.CLI/DiscordChatExporter.Cli.dll export -t   NTkzNzU2MTk0NDM5NjkyMzAw.X6RG0w.CMSNbqjw_rY04vdOlSgff9PdxbQ -c "$1"
 }
 
 function discord-download-niemieckie-slowka
 {
   discord-text-channel-download "705327376086728766"
+}
+
+function parse-info
+{
+  cat niemieckie-slowa.html | awk '{ gsub ("Avatar", "", $0) }' | html2text
 }
